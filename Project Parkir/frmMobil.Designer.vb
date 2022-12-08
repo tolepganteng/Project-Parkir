c6db81eba6e8f95cb9f130eae657273cd47106b7
tolepganteng <93376346+tolepganteng@users.noreply.github.com>
2022-12-08 11:50:22 +0700

    huhu

--- Project Parkir/frmMobil.Designer.vb
+++ Project Parkir/frmMobil.Designer.vb
@@ -22,8 +22,331 @@ Partial Class frmMobil
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.dtpCariTglJoin = New System.Windows.Forms.DateTimePicker()
+        Me.btnCariAngka = New System.Windows.Forms.Button()
+        Me.txtCariAngka = New System.Windows.Forms.TextBox()
+        Me.dgv = New System.Windows.Forms.DataGridView()
+        Me.btnCariTglJoin = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnCariNamaMember = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.lAngka = New System.Windows.Forms.Label()
+        Me.lblString = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtCariNamaMember = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCatatan = New System.Windows.Forms.TextBox()
+        Me.txtNoPolisi = New System.Windows.Forms.TextBox()
+        Me.txtMemberID = New System.Windows.Forms.TextBox()
+        Me.lblMember = New System.Windows.Forms.Label()
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dtpCariTglJoin
+        '
+        Me.dtpCariTglJoin.Location = New System.Drawing.Point(83, 72)
+        Me.dtpCariTglJoin.Name = "dtpCariTglJoin"
+        Me.dtpCariTglJoin.Size = New System.Drawing.Size(216, 20)
+        Me.dtpCariTglJoin.TabIndex = 32
+        '
+        'btnCariAngka
+        '
+        Me.btnCariAngka.Location = New System.Drawing.Point(698, 50)
+        Me.btnCariAngka.Name = "btnCariAngka"
+        Me.btnCariAngka.Size = New System.Drawing.Size(36, 20)
+        Me.btnCariAngka.TabIndex = 20
+        Me.btnCariAngka.Text = "GO"
+        Me.btnCariAngka.UseVisualStyleBackColor = True
+        '
+        'txtCariAngka
+        '
+        Me.txtCariAngka.Location = New System.Drawing.Point(476, 47)
+        Me.txtCariAngka.Name = "txtCariAngka"
+        Me.txtCariAngka.Size = New System.Drawing.Size(216, 20)
+        Me.txtCariAngka.TabIndex = 19
+        '
+        'dgv
+        '
+        Me.dgv.AllowUserToAddRows = False
+        Me.dgv.AllowUserToDeleteRows = False
+        Me.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv.Location = New System.Drawing.Point(3, 25)
+        Me.dgv.Name = "dgv"
+        Me.dgv.Size = New System.Drawing.Size(404, 229)
+        Me.dgv.TabIndex = 39
+        '
+        'btnCariTglJoin
+        '
+        Me.btnCariTglJoin.Location = New System.Drawing.Point(305, 72)
+        Me.btnCariTglJoin.Name = "btnCariTglJoin"
+        Me.btnCariTglJoin.Size = New System.Drawing.Size(47, 22)
+        Me.btnCariTglJoin.TabIndex = 34
+        Me.btnCariTglJoin.Text = "Go"
+        Me.btnCariTglJoin.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(3, 78)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(78, 13)
+        Me.Label13.TabIndex = 33
+        Me.Label13.Text = "Tanggal Daftar"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(420, 19)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(28, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Text"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(698, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(36, 20)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "GO"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(305, 49)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(36, 20)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "GO"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnCariNamaMember
+        '
+        Me.btnCariNamaMember.Location = New System.Drawing.Point(305, 8)
+        Me.btnCariNamaMember.Name = "btnCariNamaMember"
+        Me.btnCariNamaMember.Size = New System.Drawing.Size(36, 20)
+        Me.btnCariNamaMember.TabIndex = 15
+        Me.btnCariNamaMember.Text = "GO"
+        Me.btnCariNamaMember.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(476, 12)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(216, 20)
+        Me.TextBox3.TabIndex = 14
+        '
+        'lAngka
+        '
+        Me.lAngka.AutoSize = True
+        Me.lAngka.Location = New System.Drawing.Point(420, 50)
+        Me.lAngka.Name = "lAngka"
+        Me.lAngka.Size = New System.Drawing.Size(38, 13)
+        Me.lAngka.TabIndex = 13
+        Me.lAngka.Text = "Angka"
+        '
+        'lblString
+        '
+        Me.lblString.AutoSize = True
+        Me.lblString.Location = New System.Drawing.Point(7, 50)
+        Me.lblString.Name = "lblString"
+        Me.lblString.Size = New System.Drawing.Size(34, 13)
+        Me.lblString.TabIndex = 12
+        Me.lblString.Text = "String"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.btnCariTglJoin)
+        Me.Panel1.Controls.Add(Me.Label13)
+        Me.Panel1.Controls.Add(Me.dtpCariTglJoin)
+        Me.Panel1.Controls.Add(Me.btnCariAngka)
+        Me.Panel1.Controls.Add(Me.txtCariAngka)
+        Me.Panel1.Controls.Add(Me.Label10)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.btnCariNamaMember)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.lAngka)
+        Me.Panel1.Controls.Add(Me.lblString)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.txtCariNamaMember)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Location = New System.Drawing.Point(3, 277)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(759, 111)
+        Me.Panel1.TabIndex = 38
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(83, 46)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(216, 20)
+        Me.TextBox2.TabIndex = 11
+        '
+        'txtCariNamaMember
+        '
+        Me.txtCariNamaMember.Location = New System.Drawing.Point(83, 8)
+        Me.txtCariNamaMember.Name = "txtCariNamaMember"
+        Me.txtCariNamaMember.Size = New System.Drawing.Size(216, 20)
+        Me.txtCariNamaMember.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(7, 12)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(79, 13)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Nomor Member"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(687, 231)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 37
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(556, 231)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(75, 23)
+        Me.btnHapus.TabIndex = 36
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(426, 231)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(75, 23)
+        Me.btnSimpan.TabIndex = 35
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(425, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "Catatan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(425, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 13)
+        Me.Label2.TabIndex = 29
+        Me.Label2.Text = "No Polisi"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(425, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "MemberID"
+        '
+        'txtCatatan
+        '
+        Me.txtCatatan.Location = New System.Drawing.Point(546, 77)
+        Me.txtCatatan.Name = "txtCatatan"
+        Me.txtCatatan.Size = New System.Drawing.Size(216, 20)
+        Me.txtCatatan.TabIndex = 23
+        '
+        'txtNoPolisi
+        '
+        Me.txtNoPolisi.Location = New System.Drawing.Point(546, 51)
+        Me.txtNoPolisi.Name = "txtNoPolisi"
+        Me.txtNoPolisi.Size = New System.Drawing.Size(216, 20)
+        Me.txtNoPolisi.TabIndex = 22
+        '
+        'txtMemberID
+        '
+        Me.txtMemberID.Location = New System.Drawing.Point(546, 25)
+        Me.txtMemberID.Name = "txtMemberID"
+        Me.txtMemberID.Size = New System.Drawing.Size(216, 20)
+        Me.txtMemberID.TabIndex = 21
+        '
+        'lblMember
+        '
+        Me.lblMember.AutoSize = True
+        Me.lblMember.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lblMember.Location = New System.Drawing.Point(0, 0)
+        Me.lblMember.Margin = New System.Windows.Forms.Padding(10, 0, 10, 0)
+        Me.lblMember.Name = "lblMember"
+        Me.lblMember.Size = New System.Drawing.Size(50, 13)
+        Me.lblMember.TabIndex = 20
+        Me.lblMember.Text = "Members"
+        '
+        'frmMobil
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(778, 395)
+        Me.Controls.Add(Me.dgv)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCatatan)
+        Me.Controls.Add(Me.txtNoPolisi)
+        Me.Controls.Add(Me.txtMemberID)
+        Me.Controls.Add(Me.lblMember)
+        Me.Name = "frmMobil"
         Me.Text = "frmMobil"
+        CType(Me.dgv, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents dtpCariTglJoin As System.Windows.Forms.DateTimePicker
+    Friend WithEvents btnCariAngka As System.Windows.Forms.Button
+    Friend WithEvents txtCariAngka As System.Windows.Forms.TextBox
+    Friend WithEvents dgv As System.Windows.Forms.DataGridView
+    Friend WithEvents btnCariTglJoin As System.Windows.Forms.Button
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnCariNamaMember As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents lAngka As System.Windows.Forms.Label
+    Friend WithEvents lblString As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtCariNamaMember As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents btnHapus As System.Windows.Forms.Button
+    Friend WithEvents btnSimpan As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtCatatan As System.Windows.Forms.TextBox
+    Friend WithEvents txtNoPolisi As System.Windows.Forms.TextBox
+    Friend WithEvents txtMemberID As System.Windows.Forms.TextBox
+    Friend WithEvents lblMember As System.Windows.Forms.Label
 End Class
--- Project Parkir/frmMobil.vb
+++ Project Parkir/frmMobil.vb
@@ -1,3 +1,154 @@
-﻿Public Class frmMobil
+﻿Imports MySql.Data.MySqlClient
 
+Public Class frmMobil
+    Dim conn As MySqlConnection = New MySqlConnection(connString)
+    Dim connString As String = "Server=localhost;Database=mobil;Uid=root;"
+    Dim da As MySqlDataAdapter
+    Dim dr As MySqlDataReader
+    Dim sqlComm As MySqlCommand
+
+    Dim ds As DataSet
+    Dim dt As DataTable
+
+    Private Function Koneksi(ByVal reqState As Boolean) As Boolean
+        Dim ret As Boolean = True
+
+        Try
+            If reqState = True Then
+                If conn.State = ConnectionState.Closed Then conn.Open()
+            Else
+                If conn.State = ConnectionState.Open Then conn.Close()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, , "error")
+            ret = False
+        End Try
+
+        Return ret
+    End Function
+    Private Sub ResetUI()
+        txtMemberID.Clear()
+        txtNoPolisi.Clear()
+        txtCatatan.Clear()
+    End Sub
+    Private Function fillDGV() As Boolean
+        Dim ret As Boolean = True
+
+        If Koneksi(True) Then
+            Try
+                sqlComm = New MySqlCommand
+                With sqlComm
+                    .Connection = conn
+                    .CommandText = "SP_MobilSelect"
+                    .CommandType = CommandType.StoredProcedure
+
+                End With
+                dt = New DataTable
+                da = New MySqlDataAdapter
+
+                da.Fill(dt)
+                dgv.DataSource = dt
+            Catch ex As Exception
+                MsgBox(ex.Message, , "error")
+                ret = False
+            End Try
+        Else
+            ret = False
+        End If
+
+        Return ret
+    End Function
+    Private Sub SetupDGV()
+        With dgv
+            .Columns(0).HeaderText = "MemberID"
+            .Columns(1).HeaderText = "No Polisi"
+            .Columns(2).HeaderText = "Catatan"
+
+            .Columns(0).Width = 60
+            .Columns(1).Width = 200
+            .Columns(2).Width = 100
+        End With
+    End Sub
+    Private Sub frmMobil_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        fillDGV()
+        ResetUI()
+    End Sub
+
+    Private Sub btnReset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnReset.Click
+        ResetUI()
+    End Sub
+    Private Sub dgv_CellDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles dgv.CellDoubleClick
+        Dim Pilih As DataGridViewRow
+        Pilih = dgv.Rows(e.RowIndex)
+
+        Dim lKode As String
+
+        If Koneksi(True) Then
+            Try
+                sqlComm = New MySqlCommand
+                With sqlComm
+                    .Connection = conn
+                    .CommandText = "SP_MobilCariID"
+                    .CommandType = CommandType.StoredProcedure
+                    .Parameters.AddWithValue("_MemberID", lKode)
+                End With
+
+                dt = New DataTable
+                da = New MySqlDataAdapter(sqlComm)
+                da.Fill(dt)
+
+                If dt.Rows.Count = 0 Then
+                    MsgBox("Tidak Ditemukan", , "error")
+                Else
+                    For Each drow As DataRow In dt.Rows
+                        txtMemberID.Text = drow.Item("MemberID").ToString()
+                        txtNoPolisi.Text = drow.Item("No Polisi").ToString()
+                        txtCatatan.Text = drow.Item("Catatan").ToString()
+                    Next
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message, , "error")
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnSimpan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSimpan.Click
+        Dim lMemberID As String
+        Dim lNoPolisi As String
+        Dim lCatatan As String
+
+        If Koneksi(True) Then
+
+            lMemberID = txtMemberID.Text.ToString()
+            lNoPolisi = txtNoPolisi.Text.ToString()
+            lCatatan = txtCatatan.Text.ToString()
+
+
+            sqlComm = New MySqlCommand
+
+            With sqlComm
+                .Connection = conn
+                .CommandType = CommandType.StoredProcedure
+                .Parameters.AddWithValue("_MemberID", lKode)
+                .Parameters.AddWithValue("_NamaMember", lNamaMember)
+                .Parameters.AddWithValue("_Alamat", lAlamat)
+
+                If lKode = "" Then
+                    .CommandText = "SP_MemberInsert"
+                Else
+                    .CommandText = "SP_MemberUpdate"
+                End If
+
+                If .ExecuteNonQuery() = 1 Then
+                    MsgBox("Berhasil", , "info")
+                Else
+                    MsgBox("Gagal", , "info")
+                End If
+            End With
+            fillDGV()
+            Catch ex As Exception
+                MsgBox(ex.Message, , "error")
+            End Try
+        End If
+    End Sub
 End Class
